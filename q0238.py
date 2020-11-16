dc83d4439c13886957752877d47d1e730d7587db
Yuxi Liu <yuxiliuasana@fb.com>
2020-11-15 19:12:14 -0500

    238

--- q0238.py
+++ q0238.py
@@ -1,6 +1,15 @@
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        i = 1
-        for n in nums:
-            i *= n
-        return [int(i/n) for n in nums]
+        product = 1
+        zero_index = []
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                zero_index += [i]
+            else:
+                product *= nums[i]
+        if len(zero_index) > 1:
+            return [0] * len(nums)
+        if len(zero_index) == 1:
+            return [0] * zero_index[0] + [product] + [0] * (len(nums) - zero_index[0] - 1)
+
+        return [int(product / n) for n in nums]
